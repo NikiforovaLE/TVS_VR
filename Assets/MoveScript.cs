80f768f801cd218143c41e702d6c7053dc8f6d5b
asp.Denis Kovalenok <faith.science.denis.kovalionok@gmail.com>
2022-06-09 18:08:29 +0300

    Create Project

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position += new Vector3(-0.6f, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
---
+++ Assets/MoveTvs.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveTvs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int k = 0;
+    public GameObject Хватайка;
+
+    public Text хватайка;
+    public Text нога;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > 16)
+            k = -1;
+        if (transform.position.z < 8)
+            k = 1;
+        
+        transform.position += Vector3.forward * k* Time.deltaTime;
+
+        if (Хватайка.transform.position.y < 10)
+        Хватайка.transform.position += Vector3.up * k * Time.deltaTime;
+
+
+        хватайка.text = Хватайка.transform.position.y.ToString();
+        нога.text = Хватайка.transform.position.z.ToString();
+    }
+}
