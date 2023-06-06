e03931c13d1159fcdc58531b3ada70036347f85a
NikiforovaLE <n.lubovj@gmail.com>
2023-06-06 23:05:15 +0300

    Improve Mnemo11Animation script

--- Assets/Scripts/Mnemo11Animation.cs
+++ Assets/Scripts/Mnemo11Animation.cs
@@ -6,7 +6,14 @@ using UnityEngine.UI;
 public class Mnemo11Animation : MonoBehaviour
 {
     [SerializeField] private Text currentTVS;
+    [SerializeField] private Text performedText;
+    [SerializeField] private Image suitableButton;
+    [SerializeField] private Image defectiveButton;
+
     public ARM2Mnemo0 ARM2Mnemo0;
+    private Animator currentAnimator;
+
+    private readonly string whenPerformed = "выполнен";
     private bool isSuitable = false;
 
     public bool IsSuitable { get => isSuitable; set => isSuitable = value; }
@@ -15,6 +22,7 @@ public class Mnemo11Animation : MonoBehaviour
     void Start()
     {
         currentTVS.text = "";
+        currentAnimator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,13 +31,29 @@ public class Mnemo11Animation : MonoBehaviour
         currentTVS.text = ARM2Mnemo0.FrameNumber.text;
     }
 
+    public void SetWasPerfomedTrue()
+    {
+        currentAnimator.SetBool("wasPerfomed", true);
+        performedText.text = whenPerformed;
+    }
+
     public void SetSuitable()
     {
-        IsSuitable = true;
+        if (currentAnimator.GetBool("wasPerfomed"))
+        {
+            IsSuitable = true;
+            suitableButton.color = Color.green;
+            defectiveButton.color = Color.white;
+        }
     }
 
     public void SetDefective()
     {
-        IsSuitable = false;
+        if (currentAnimator.GetBool("wasPerfomed"))
+        {
+            IsSuitable = false;
+            suitableButton.color = Color.white;
+            defectiveButton.color = Color.red;
+        }
     }
 }
