905071b0f183a54690efae51ffaffce2f4be75a2
NikiforovaLE <n.lubovj@gmail.com>
2023-06-08 17:23:58 +0300

    Correct Mnemo12Animation

--- Assets/Scripts/Mnemo12Animation.cs
+++ Assets/Scripts/Mnemo12Animation.cs
@@ -3,16 +3,56 @@ using UnityEngine.UI;
 
 public class Mnemo12Animation : MonoBehaviour
 {
+    [SerializeField] private Text step;
+    [SerializeField] private Text tvsWeightLossText;
+    [SerializeField] private Text suzWeightLossText;
     [SerializeField] private Text weightControlIsDoneText;
     [SerializeField] private Text inputControlIsDoneText;
     [SerializeField] private Text lockControlIsDoneText;
     [SerializeField] private Text suzControlIsDoneText;
 
+    private Animator currentAnimator;
     private bool isChecked;
 
     private readonly string done = "Выполнен";
 
     public bool IsChecked { get => isChecked; set => isChecked = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAnimator = gameObject.GetComponent<Animator>();
+    }
+
+    public void StartControl()
+    {
+        currentAnimator.Play("12 Mnemo Animation");
+    }
+
+    public void SetStep1()
+    {
+        step.text = "1";
+    }
+
+    public void SetStep2()
+    {
+        step.text = "2";
+    }
+
+    public void SetStep3()
+    {
+        step.text = "3";
+    }
+
+    public void SetTvsWeightLoss()
+    {
+        tvsWeightLossText.text = "230";
+    }
+
+    public void SetSuzWeightLoss()
+    {
+        suzWeightLossText.text = "60";
+    }
 
     private void IsDone(Text isDoneText)
     {
