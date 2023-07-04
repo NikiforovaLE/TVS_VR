9548c34aad29d792b97fbd247dd181328e8af489
NikiforovaLE <100089594+NikiforovaLE@users.noreply.github.com>
2023-07-04 15:55:52 +0300

    Work with 12 and 13 mnemo animation

--- Assets/Scripts/Mnemo13Animation.cs
+++ Assets/Scripts/Mnemo13Animation.cs
@@ -1,43 +1,64 @@
 using UnityEngine;
 using UnityEngine.UI;
+using Oculus.Interaction;
 
 public class Mnemo13Animation : MonoBehaviour
 {
     [SerializeField] private Text lengthControlIsDoneText;
     [SerializeField] private Text sizeControlIsDoneText;
     [SerializeField] private Text viewControlIsDoneText;
+    [SerializeField] private Text attentionMessage;
+
+    [SerializeField] private Image isPerformingBackgrond;
 
     [SerializeField] private Mnemo12Animation mnemo12Animation;
 
     [SerializeField] private Animator mnemo14Animator;
-    [SerializeField] private Text attentionMessage;
-    private bool isChecked;
 
+    [SerializeField] private GeneralPanelAcions generalPanelAcions;
+
+    [SerializeField] private GameObject mnemo14;
+    [SerializeField] private GameObject isSuitableButton;
+    [SerializeField] private GameObject defectButton;
+
+    private Animator currentAnimator;
+    private bool mnemo12isChecked;
+    private bool mnemo13IsChecked;
+
+    private readonly Color whenIsNotPerforming = new Color(0f, 255f, 0f, 255f);
+    private readonly Color whenIsPerforming = new Color(0f, 255f, 0f, 255f);
     private readonly string done = "Выполнен";
     private readonly string messageWhenNotChecked = "Проведите проверку";
 
-    public void isSuitable()
-    {
-        if (isChecked)
-        {
-            attentionMessage.text = "";
-            mnemo14Animator.Play("14 Mnemo Animation");
-        } else
-        {
-            attentionMessage.text = messageWhenNotChecked;
-        }
-    }
     // Start is called before the first frame update
     void Start()
     {
-        isChecked = false;
+        mnemo13IsChecked = false;
         attentionMessage.text = "";
+        currentAnimator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        isChecked = mnemo12Animation.IsChecked;
+        mnemo12isChecked = mnemo12Animation.IsChecked;
+    }
+
+    public void StartControl()
+    {
+        if (mnemo12isChecked)
+        {
+            currentAnimator.Play("13 Mnemo Animation");
+            MakeButtonsUnPointable();
+            attentionMessage.text = "";
+            isPerformingBackgrond.color = whenIsPerforming;
+        }
+    }
+
+    private void MakeButtonsUnPointable()
+    {
+        isSuitableButton.GetComponent<PointableUnityEventWrapper>().enabled = false;
+        defectButton.GetComponent<PointableUnityEventWrapper>().enabled = false;
     }
 
     private void IsDone(Text isDoneText)
@@ -58,5 +79,30 @@ public class Mnemo13Animation : MonoBehaviour
     public void SetViewControlIsDoneText()
     {
         IsDone(viewControlIsDoneText);
+    }
+
+    public void MakeButtonsPointable()
+    {
+        isSuitableButton.GetComponent<PointableUnityEventWrapper>().enabled = true;
+        defectButton.GetComponent<PointableUnityEventWrapper>().enabled = true;
+    }
+
+    public void MakeMnemo13IsCheckedTrue()
+    {
+        mnemo13IsChecked = true;
+        isPerformingBackgrond.color = whenIsNotPerforming;
+    }
+
+    public void IsSuitable()
+    {
+        if (mnemo13IsChecked)
+        {
+            attentionMessage.text = "";
+            generalPanelAcions.SetPanelActive(mnemo14);
+        }
+        else
+        {
+            attentionMessage.text = messageWhenNotChecked;
+        }
     }
 }
