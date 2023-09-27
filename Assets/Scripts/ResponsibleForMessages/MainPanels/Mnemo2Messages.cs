67bb6540f3643ed8bbe43b23cf2a475be329cf7d
NikiforovaLE <100089594+NikiforovaLE@users.noreply.github.com>
2023-09-27 21:53:03 +0300

    Correct Mnemo03Animation, Mnemo02Animation and Mnemo04Animation

--- Assets/Scripts/ResponsibleForMessages/MainPanels/Mnemo2Messages.cs
+++ Assets/Scripts/ResponsibleForMessages/MainPanels/Mnemo2Messages.cs
@@ -4,24 +4,31 @@ public class Mnemo2Messages : MonoBehaviour
 {
     [SerializeField] private MessageInfo messageInfoOnMnemo2;
 
-    private readonly string openShiberCommand = "Выдана команда \"Открыть шибер\"";
-    private readonly string closeShiberCommand = "Выдана команда \"Закрыть шибер\"";
+    private readonly string openShiberCommandText = "Выдана команда \"Открыть шибер\"";
+    private readonly string closeShiberCommandText = "Выдана команда \"Закрыть шибер\"";
+    private readonly string giveVtukCommandText = "Выдана команда \"Подать ВТУК\"";
 
     private readonly string shiberIsOpened = "Шибер открыт";
     private readonly string shiberIsClosed = "Шибер закрыт";
+    private readonly string vtukIsGiven = "ВТУК подан";
 
     private readonly string positiveStatus = "+";
     private readonly string negativeStatus = "-";
 
     private readonly string messageSource = "Работа с ВТУК";
-    public void OpenShiber()
+    public void OpenShiberCommamd()
     {
-        messageInfoOnMnemo2.FillInfo(openShiberCommand, messageSource, positiveStatus);
+        messageInfoOnMnemo2.FillInfo(openShiberCommandText, messageSource, positiveStatus);
     }
 
-    public void CloseShiber()
+    public void CloseShiberCommamd()
     {
-        messageInfoOnMnemo2.FillInfo(closeShiberCommand, messageSource, positiveStatus);
+        messageInfoOnMnemo2.FillInfo(closeShiberCommandText, messageSource, positiveStatus);
+    }
+    
+    public void GiveVtukCommand()
+    {
+        messageInfoOnMnemo2.FillInfo(giveVtukCommandText, messageSource, positiveStatus);
     }
 
     public void SetShiberIsOpened()
@@ -32,5 +39,10 @@ public class Mnemo2Messages : MonoBehaviour
     public void SetShiberIsClosed()
     {
         messageInfoOnMnemo2.FillInfo(shiberIsClosed, messageSource, positiveStatus);
+    }
+    
+    public void SetVtukIsGiven()
+    {
+        messageInfoOnMnemo2.FillInfo(vtukIsGiven, messageSource, positiveStatus);
     }
 }
