e07627287043f188ae213a5d687c497bd0159514
NikiforovaLE <100089594+NikiforovaLE@users.noreply.github.com>
2023-10-10 16:29:27 +0300

    Refactor project. Work with Mnemo15

--- Assets/Scripts/ResponsibleForMessages/MainPanels/Mnemo15Messages.cs
+++ Assets/Scripts/ResponsibleForMessages/MainPanels/Mnemo15Messages.cs
@@ -1,8 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Mnemo15Messages : MonoBehaviour
 {
     [SerializeField] private MessageInfo messageInfoOnMnemo15;
+
+    private readonly string moveTransportCarToManipulatorCranZoneCommand = "Выдана команда \"Переместить тележку передаточную в зону \"Кран-манипулятор\"\"";
+    private readonly string moveTransportCarToBoxLidRemovingZoneCommand = "Выдана команда \"Переместить тележку передаточную в зону \"Снятие крышки пенала\"\"";
+    private readonly string moveTransportCarToCoordinateManipulatorZoneCommand = "Выдана команда \"Переместить тележку передаточную в зону \"Координатный манипулятор\"\"";
+
+    private readonly string positiveStatus = "+";
+    private readonly string negativeStatus = "-";
+
+    private readonly string messageSourceIsEmptyBoxTransportation = "Транспортировка порожнего пенала";
+    private readonly string messageSourceIsBoxedTvsTransportation = "Транспортировка запеналенной ТВС";
+    public void MoveTransportCarToManipulatorCranZoneCommand()
+    {
+        messageInfoOnMnemo15.FillInfo(moveTransportCarToManipulatorCranZoneCommand, messageSourceIsBoxedTvsTransportation, positiveStatus);
+    }
+    
+    public void MoveTransportCarToBoxLidRemovingZoneCommand()
+    {
+        messageInfoOnMnemo15.FillInfo(moveTransportCarToBoxLidRemovingZoneCommand, messageSourceIsEmptyBoxTransportation, positiveStatus);
+    }
+    
+    public void MoveTransportCarToCoordinateManipulatorZoneCommand()
+    {
+        messageInfoOnMnemo15.FillInfo(moveTransportCarToCoordinateManipulatorZoneCommand, messageSourceIsEmptyBoxTransportation, positiveStatus);
+    }
 }
