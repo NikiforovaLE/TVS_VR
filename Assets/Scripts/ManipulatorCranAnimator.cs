798d2ad9d8e9f36b895ce7f19a5928ec64bbfbba
Lyubov <n.lubovj@gmail.com>
2023-10-27 16:43:21 +0300

    Fix ManipulatorCran and TransportTruck animation

--- Assets/Scripts/ManipulatorCranAnimator.cs
+++ Assets/Scripts/ManipulatorCranAnimator.cs
@@ -1,24 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ManipulatorCranAnimator : MonoBehaviour
 {
+    [SerializeField] private GameObject transportTruckBox;
+    [SerializeField] private GameObject transportTruckBoxLid;
+    [SerializeField] private GameObject manipulatorCranBox;
     private Animator currentAnimator;
+
     public void StopAnimation()
     {
         currentAnimator.speed = 0;
+    }
+
+    public void MakeTransportTruckBoxVisibleManipulatorCranBoxInvisible()
+    {
+        transportTruckBoxLid.SetActive(true);
+        transportTruckBox.SetActive(true);
+        manipulatorCranBox.SetActive(false);
     }
 
     // Start is called before the first frame update
     void Start()
     {
         currentAnimator = gameObject.GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
