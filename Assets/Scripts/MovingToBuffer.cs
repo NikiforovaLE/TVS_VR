1c9b519c3f17e4eac90ebd1b896d69bbb0000a22
NikiforovaLE <100089594+NikiforovaLE@users.noreply.github.com>
2023-11-16 20:38:16 +0300

    Add moving from buffer

--- Assets/Scripts/MovingToBuffer.cs
+++ Assets/Scripts/MovingToBuffer.cs
@@ -3,33 +3,35 @@ using UnityEngine;
 
 public class MovingToBuffer : MonoBehaviour
 {
-    [SerializeField] GameObject mnemo08;
-    [SerializeField] AnimationClip mnemo08Washing;
-    [SerializeField] AnimationClip mnemo08Drying;
-    [SerializeField] AnimationClip mnemo08Tightness;
-    [SerializeField] AnimationClip mnemo08Impurity;
-    [SerializeField] AnimationClip mnemo08Weight;
-    [SerializeField] AnimationClip mnemo08Geometry;
+    [SerializeField] private AnimationClip mnemo08Washing;
+    [SerializeField] private AnimationClip mnemo08Drying;
+    [SerializeField] private AnimationClip mnemo08Tightness;
+    [SerializeField] private AnimationClip mnemo08Impurity;
+    [SerializeField] private AnimationClip mnemo08Weight;
+    [SerializeField] private AnimationClip mnemo08Geometry;
+    [SerializeField] private Animator coordinateManipulatorAnimator;
 
     private Animator mnemo08Animator;
     private List<AnimationClip> mnemo08Animation;
+
+    private string requiredBufferAnimation = "";
+
+    public string RequiredBufferAnimation { get => requiredBufferAnimation; set => requiredBufferAnimation = value; }
 
     public void MoveToBuffer()
     {
         string currentAnimationName = mnemo08Animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
         AnimationClip currentAnimation = mnemo08Washing;
-        string bufferAnimationName = "";
         foreach (AnimationClip animation in mnemo08Animation)
         {
             if (animation.name.Equals(currentAnimationName))
             {
                 currentAnimation = animation;
-                bufferAnimationName = GetBufferAnimationName(currentAnimationName);
+                requiredBufferAnimation = GetBufferAnimationName(currentAnimationName);
                 break;
             }
         }
-        StartCoroutine(waiter(currentAnimation, bufferAnimationName));
-
+        StartCoroutine(waiter(currentAnimation, requiredBufferAnimation));
     }
 
     System.Collections.IEnumerator waiter(AnimationClip currentAnimation, string bufferAnimationName)
@@ -37,6 +39,7 @@ public class MovingToBuffer : MonoBehaviour
         //wait untill current animation is finished
         yield return new WaitForSeconds(currentAnimation.length - mnemo08Animator.GetCurrentAnimatorStateInfo(0).normalizedTime * mnemo08Animator.GetCurrentAnimatorStateInfo(0).length - 1);
         mnemo08Animator.Play(bufferAnimationName);
+        coordinateManipulatorAnimator.Play(bufferAnimationName);
     }
 
     private string GetBufferAnimationName(string currentAnimationName)
@@ -56,7 +59,7 @@ public class MovingToBuffer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mnemo08Animator = mnemo08.GetComponent<Animator>();
+        mnemo08Animator = gameObject.GetComponent<Animator>();
         mnemo08Animation = new List<AnimationClip> { mnemo08Washing, mnemo08Drying, mnemo08Tightness, mnemo08Impurity, mnemo08Weight, mnemo08Weight };
     }
 }
