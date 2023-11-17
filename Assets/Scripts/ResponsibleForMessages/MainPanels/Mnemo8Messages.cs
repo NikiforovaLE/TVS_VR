58dd4739edffd7c327d18c81289353724ffcd9b5
NikiforovaLE <100089594+NikiforovaLE@users.noreply.github.com>
2023-11-17 18:10:59 +0300

    Correct Mnemo8Messages script

--- Assets/Scripts/ResponsibleForMessages/MainPanels/Mnemo8Messages.cs
+++ Assets/Scripts/ResponsibleForMessages/MainPanels/Mnemo8Messages.cs
@@ -29,8 +29,98 @@ public class Mnemo8Messages : MonoBehaviour
     private readonly string dryingRetortIsClosed = "Крышка реторты сушки закрыта";
     private readonly string tightnessRetortIsClosed = "Крышка реторты герметичности закрыта";
 
-    private readonly string messageSourceIsAssembly = "Сборка ТВС";
+    private readonly string messageSourceIsCoordinateManipulator = "Координатный некопирующий манипулятор";
 
     private readonly string positiveStatus = "+";
     private readonly string negativeStatus = "-";
+
+    public void MoveTvsToWashingCommand()
+    {
+        messageInfoOnMnemo8.FillInfo(moveTvsToWashingCommand, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+
+    public void MoveTvsToDryingCommand()
+    {
+        messageInfoOnMnemo8.FillInfo(moveTvsToDryingCommand, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void MoveTvsToTightnessControlCommand()
+    {
+        messageInfoOnMnemo8.FillInfo(moveTvsToTightnessControlCommand, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void MoveTvsToImpurityControlCommand()
+    {
+        messageInfoOnMnemo8.FillInfo(moveTvsToImpurityControlCommand, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void MoveTvsToWeightControlCommand()
+    {
+        messageInfoOnMnemo8.FillInfo(moveTvsToWeightControlCommand, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void MoveTvsToGeometryControlCommand()
+    {
+        messageInfoOnMnemo8.FillInfo(moveTvsToGeometryControlCommand, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void OpenWashRetortCommand()
+    {
+        messageInfoOnMnemo8.FillInfo(openWashRetortCommand, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void OpenDryingRetortCommand()
+    {
+        messageInfoOnMnemo8.FillInfo(openDryingRetortCommand, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void OpenTightnessRetortCommand()
+    {
+        messageInfoOnMnemo8.FillInfo(openTightnessRetortCommand, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void SetWashRetortIsOpened()
+    {
+        messageInfoOnMnemo8.FillInfo(washRetortIsOpened, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void SetDryingRetortIsOpened()
+    {
+        messageInfoOnMnemo8.FillInfo(dryingRetortIsOpened, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void SetTightnessRetortIsOpened()
+    {
+        messageInfoOnMnemo8.FillInfo(tightnessRetortIsOpened, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void CloseWashRetortCommand()
+    {
+        messageInfoOnMnemo8.FillInfo(closeWashRetortCommand, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void CloseDryingRetortCommand()
+    {
+        messageInfoOnMnemo8.FillInfo(closeDryingRetortCommand, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void CloseTightnessRetortCommand()
+    {
+        messageInfoOnMnemo8.FillInfo(closeTightnessRetortCommand, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void SetWashRetortIsClosed()
+    {
+        messageInfoOnMnemo8.FillInfo(washRetortIsClosed, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void SetDryingRetortIsClosed()
+    {
+        messageInfoOnMnemo8.FillInfo(dryingRetortIsClosed, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
+    
+    public void SetTightnessRetortIsClosed()
+    {
+        messageInfoOnMnemo8.FillInfo(tightnessRetortIsClosed, messageSourceIsCoordinateManipulator, positiveStatus);
+    }
 }
